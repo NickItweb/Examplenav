34b2263be35c1510644a072553cf3b069c06e591
NickItweb <98798052+NickItweb@users.noreply.github.com>
2022-03-17 10:29:31 +0530

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,45 @@
+<script>
+import FeedbackList from '/public/components/FeedbackList.svelte'
+
+  let feedback = [
+    {
+      id:1,
+      rating:10,
+      text:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown',
+    },
+    {
+      id:2,
+      rating:1,
+      text:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown',
+    },
+    {
+      id:3,
+      rating:4,
+      text:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown',
+    }
+  ]
+
+  const deleteFeedback = (e) =>{
+    const itemId = e.detail
+    feedback = feedback.filter((item) => item.id != itemId)
+  }
+</script>
+
+<style>
+main{
+   background:black;  
+   
+}
+  .container {
+   max-width:768px;
+    margin:100px auto;
+    padding:20px;
+    
+  }
+ 
+</style>
+
+<main class="container">
+	<FeedbackList {feedback} on:delete-feedback = {deleteFeedback} />
+	
+</main>
---
+++ public/components/FeedbackItem.svelte
@@ -0,0 +1,46 @@
+<script>
+  import {createEventDispatcher} from 'svelte'
+  import Card from './Card.svelte'
+  export let item
+
+const dispatch = createEventDispatcher()
+
+  const handleDelete =(itemId) => {
+    dispatch('delete-feedback',itemId)
+  }
+</script>
+
+<Card>
+  <div class="num-display">
+    {item.rating}
+  </div>
+  <button class="close" on:click={() => handleDelete(item.id)}>X</button>
+  <p class="text-display">
+    {item.text}  
+  </p>
+</Card>
+
+<style>
+  .num-display{
+    position:absolute;
+    top:-10px;
+    left:-10px;
+    width:20px;
+    height:20px;
+    text-align:center;
+    padding:10px;
+    background:red;
+    color:#fff;
+    border:1px #eee solid;
+    border-radius:50%;
+
+  }
+  .close{
+    position:absolute;
+    top:10px;
+    right:10px;
+    cursor:pointer;
+    font-size:20px ;
+    font-weight:600;
+  }
+</style>
